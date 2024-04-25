26bbf8c899b242890ec1e6564c993cdda4ae3d00
Yash Agrawal <yash17agrawal@gmail.com>
2024-04-25 11:42:01 +0530

    Added retry mechanism

--- fastapi_demo/Parser.py
+++ fastapi_demo/Parser.py
@@ -25,54 +25,59 @@ class Parser:
         if retry_count:
             self.retry_count = retry_count
     
-    def _scrape(self, url, products) -> List[Product]:
-        response = requests.get(url)
-        if response.status_code == 200:
-        # Parse the HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
+    def _scrape(self, url, products, attempt=1) -> List[Product]:
+        print("Parsing {}, attempt: {}".format(url, attempt))
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                # Parse the HTML content
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-            # Extract product information
+                # Find all product elements on the page
+                product_cards = soup.find_all('div', class_='product-inner')
+                
+                for card in product_cards:
+                    thumbnail = card.find('img', class_='attachment-woocommerce_thumbnail')["data-lazy-src"]
+                    # Extract product name
+                    title = card.find('h2', class_='woo-loop-product__title').find('a').text.strip()
+                    # Extract product price
+                    price = card.find('ins')
+                    if price:
+                        price = price.find('span').find("bdi").text.strip()
+                    else:
+                        price = card.find('span', class_='woocommerce-Price-amount').find('bdi').text.strip()
+                    # Remove the currency symbol (₹) and any non-numeric characters
+                    numeric_value = ''.join(filter(str.isdigit, price))
 
-            # Find all product elements on the page
-            product_cards = soup.find_all('div', class_='product-inner')
-            
-            for card in product_cards:
-                thumbnail = card.find('img', class_='attachment-woocommerce_thumbnail')["data-lazy-src"]
-                # Extract product name
-                title = card.find('h2', class_='woo-loop-product__title').find('a').text.strip()
-                # Extract product price
-                price = card.find('ins')
-                if price:
-                    price = price.find('span').find("bdi").text.strip()
-                else:
-                    price = card.find('span', class_='woocommerce-Price-amount').find('bdi').text.strip()
-                # Remove the currency symbol (₹) and any non-numeric characters
-                numeric_value = ''.join(filter(str.isdigit, price))
-
-                # Convert the numeric value to an integer
-                price = int(numeric_value)
+                    # Convert the numeric value to an integer
+                    price = int(numeric_value)
 
 
-                # Append product information to the list
-                products.append({'name': title, 'price': price, 'image_url': thumbnail})
-            if products:
-                self.store_data(products)
-            # Find the link to the next page
-            pagination = soup.find("nav", class_="woocommerce-pagination").find("ul").find_all("li")
-            for line in pagination:
-                current_page = line.find('span', class_='current')
-                if current_page:
-                    current_page = int(current_page.text.strip())
-                    if current_page <= self.number_of_pages:
-                        # Get the URL of the next page
-                        next_page_url = line.find_next_sibling().find("a")['href']
-                        print("Parsing {}".format(next_page_url))
-                        # Make an HTTP request to the URL of the next page
-                        return self._scrape(next_page_url, products)
-                    return
-        else:
-            # Print an error message if the request was unsuccessful
-            print(f"Failed to retrieve data from {self.remote_url}. Status code: {response.status_code}")
+                    # Append product information to the list
+                    products.append({'name': title, 'price': price, 'image_url': thumbnail})
+                if products:
+                    self.store_data(products)
+                # Find the link to the next page
+                pagination = soup.find("nav", class_="woocommerce-pagination").find("ul").find_all("li")
+                for line in pagination:
+                    current_page = line.find('span', class_='current')
+                    if current_page:
+                        current_page = int(current_page.text.strip())
+                        if current_page < self.number_of_pages:
+                            # Get the URL of the next page
+                            next_page_url = line.find_next_sibling().find("a")['href']
+                            # Make an HTTP request to the URL of the next page
+                            return self._scrape(next_page_url, products)
+                        return
+            else:
+                raise Exception(f"Failed to retrieve data from {self.remote_url}. Status code: {response.status_code}")
+        except Exception as e:
+            print(e)
+            if attempt < self.retry_count:
+                # Retry the request if it fails
+                return self._scrape(url, products, attempt + 1)
+            else:
+                print("Failed to retrieve data from {}. Max retries exceeded.".format(url)) 
 
 
     def scrape(self) -> List[Product]:
@@ -80,9 +85,9 @@ class Parser:
         """
             Scrape the remote URL and stores the data in catalogue_data.json
         """
-        # catalogue_data = [{"name": "John", "price": 30, "img_url": "https://1.com/shop/"},
-        #                   {"name": "Alice", "price": 25, "img_url": "https://2.com/shop/"},
-        #                   {"name": "Alice", "price": 0, "img_url": "https://2.com/shop/"}]
+        # products = [{"name": "John", "price": 30, "image_url": "https://1.com/shop/"},
+        #                   {"name": "Alice", "price": 25, "image_url": "https://2.com/shop/"},
+        #                   {"name": "Alice", "price": 0, "image_url": "https://2.com/shop/"}]
         self._scrape(self.remote_url, products)
         
 
