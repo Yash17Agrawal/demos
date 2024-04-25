75176915bb7d54155db969e6778cf31479eee394
Yash Agrawal <yash17agrawal@gmail.com>
2024-04-25 08:13:49 +0530

    Added beautiful soup parser

--- fastapi_demo/Parser.py
+++ fastapi_demo/Parser.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from bs4 import BeautifulSoup
+import requests
 from Product import Product
 
 import json
@@ -21,26 +25,59 @@ class Parser:
         if retry_count:
             self.retry_count = retry_count
 
-    def scrape(self) -> list[Product]:
+    def scrape(self) -> List[Product]:
         """
             Scrape the remote URL and stores the data in catalogue_data.json
         """
-        catalogue_data = [{"name": "John", "price": 30, "img_url": "https://1.com/shop/"},
-                          {"name": "Alice", "price": 25, "img_url": "https://2.com/shop/"},
-                          {"name": "Alice", "price": 0, "img_url": "https://2.com/shop/"}]
-        if catalogue_data:
-            self.store_data(catalogue_data)
-        return 'hello world'
+        # catalogue_data = [{"name": "John", "price": 30, "img_url": "https://1.com/shop/"},
+        #                   {"name": "Alice", "price": 25, "img_url": "https://2.com/shop/"},
+        #                   {"name": "Alice", "price": 0, "img_url": "https://2.com/shop/"}]
+        
+        response = requests.get(self.remote_url)
+        if response.status_code == 200:
+        # Parse the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-    def store_data(self, data: list) -> None:
-        valid_scrapped_products: list[Product] = []
-        invalid_scrapped_products: list[dict] = []
+            # Extract product information
+            products = []
+
+            # Find all product elements on the page
+            product_cards = soup.find_all('div', class_='product-inner')
+
+            for card in product_cards[:1]:
+                thumbnail = card.find('img', class_='attachment-woocommerce_thumbnail')["data-lazy-src"]
+                # Extract product name
+                title = card.find('h2', class_='woo-loop-product__title').find('a').text.strip()
+                # Extract product price
+                price = card.find('ins').find('span').find("bdi").text.strip()
+
+                # Remove the currency symbol (₹) and any non-numeric characters
+                numeric_value = ''.join(filter(str.isdigit, price))
+
+                # Convert the numeric value to an integer
+                price = int(numeric_value)
+
+
+                # Append product information to the list
+                products.append({'name': title, 'price': price, 'image_url': thumbnail})
+
+            print(products)
+        else:
+            # Print an error message if the request was unsuccessful
+            print(f"Failed to retrieve data from {self.remote_url}. Status code: {response.status_code}")
+        if products:
+            self.store_data(products)
+
+    def store_data(self, data: List[dict]) -> None:
+        valid_scrapped_products: List[Product] = []
+        invalid_scrapped_products: List[dict] = []
         for product_items in data:
             try:
                 product = Product(
-                    product_items['name'], product_items['price'], product_items['img_url'])
+                    product_items['name'], product_items['price'], product_items['image_url'])
                 valid_scrapped_products.append(product)
             except Exception as e:
+                print(e)
                 invalid_scrapped_products.append(product_items)
 
         # Save scraped data to a JSON file
@@ -48,10 +85,10 @@ class Parser:
             products_dict = [product.to_dict()
                              for product in valid_scrapped_products]
             json.dump(products_dict, json_file, indent=4)
-        print("Catalogue data saved to catalogue_data.json")
         self._notify(len(valid_scrapped_products),
                      len(invalid_scrapped_products))
 
     def _notify(self, success_count, failed_count) -> None:
+        print("Catalogue data saved to catalogue_data.json")
         print("{} products scrapped successfully and {} failed".format(
             success_count, failed_count))
